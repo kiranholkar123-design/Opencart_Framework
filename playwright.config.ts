183fa42d408da4e1578a04b1b6de03a0c9b60845
KIRANholkar <101033581+KIRANholkar@users.noreply.github.com>
2026-09-11 18:32:00 +0530

    yml updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,12 +43,12 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: {
-    //     ...devices['Desktop Chrome'],
-    //   },
-    // },
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
 
     // {
     //   name: 'firefox',
