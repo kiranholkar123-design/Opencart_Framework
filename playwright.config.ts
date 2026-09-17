16f722966477e6ce908b5e758ac03a63ffd0ada9
KIRANholkar <101033581+KIRANholkar@users.noreply.github.com>
2026-09-17 09:58:31 +0530

    mark headless true for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     actionTimeout: 30_000,
     navigationTimeout: 10_000,
 
-    headless: false,
+    headless: process.env ? true : false,
     video: 'retain-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
